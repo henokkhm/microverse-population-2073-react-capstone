507f3f7451b860e107d92a6bd42280c421ecadc9
Henok Kirubel <henokhm2@gmail.com>
2023-08-15 19:02:02 +0300

    Clean up create-react-app boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/population 2073/i);
   expect(linkElement).toBeInTheDocument();
 });
