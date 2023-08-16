766f98b45a8e09d81cb702d843eee747e7786b34
Henok Kirubel <henokhm2@gmail.com>
2023-08-16 12:34:28 +0300

    Add region redux slice

---
+++ src/redux/region/regionSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import regionsList from '../../data/regions.json';
+
+const initialState = {
+  regionData: {},
+  isLoadingRegionData: true,
+  errorLoadingRegionData: false,
+};
+
+export const getRegionData = createAsyncThunk(
+  'regions/getRegionData',
+  async (regionId) => {
+    const regionData = regionsList.find((region) => region.id === regionId);
+    return new Promise((resolve) => {
+      resolve(regionData);
+    });
+  },
+);
+
+const regionSlice = createSlice({
+  name: 'region',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRegionData.pending, (state) => {
+        state.isLoadingRegionData = true;
+      })
+      .addCase(getRegionData.fulfilled, (state, action) => {
+        state.isLoadingRegionData = false;
+        state.regionData = action.payload;
+        state.errorLoadingRegionData = false;
+      })
+      .addCase(getRegionData.rejected, (state) => {
+        state.isLoadingRegionData = false;
+        state.errorLoadingRegionData = true;
+      });
+  },
+});
+
+export default regionSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import worldSlice from './world/worldSlice';
+import regionSlice from './region/regionSlice';
 
 const store = configureStore({
   reducer: {
     world: worldSlice,
+    region: regionSlice,
   },
 });
 
