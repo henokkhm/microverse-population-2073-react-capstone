ea9c13d5c8d4e1f00d47f9dde5b360b859fa9fb7
Henok Kirubel <henokhm2@gmail.com>
2023-08-17 00:59:16 +0300

    Add ContinentPage component and logic to fetch data when the continent page is loaded

--- src/components/countries/CountriesList.js
+++ src/components/countries/CountriesList.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import Country from './Country';
 import styles from '../../styles/CountriesList.module.css';
 
-function CountriesList({ countryData }) {
+function CountriesList({ regionId, regionName, countries }) {
   return (
     <div className={styles.countriesListWrapper}>
-      <h2 className={styles.countriesListHeader}>{`${countryData.region} Population in 2073`}</h2>
-      <ul className={styles.countriesList}>
-        {countryData.countries.map((country) => (
+      <h2 className={styles.countriesListHeader}>
+        {`${regionName} Population in 2073`}
+      </h2>
+      <ul className={styles.countriesList} data-region-id={regionId}>
+        {countries.map((country) => (
           <Country
             key={country.id}
             name={country.name}
@@ -20,17 +22,16 @@ function CountriesList({ countryData }) {
   );
 }
 
-const countriesListShape = PropTypes.shape({
+const countriesArrayShape = PropTypes.shape({
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   populationIn2073: PropTypes.number.isRequired,
 });
 
 CountriesList.propTypes = {
-  countryData: PropTypes.shape({
-    region: PropTypes.string.isRequired,
-    countries: PropTypes.arrayOf(countriesListShape).isRequired,
-  }).isRequired,
+  regionId: PropTypes.string.isRequired,
+  regionName: PropTypes.string.isRequired,
+  countries: PropTypes.arrayOf(countriesArrayShape).isRequired,
 };
 
 export default CountriesList;
--- src/pages/ContinentPage.js
+++ src/pages/ContinentPage.js
@@ -1,5 +1,46 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import { getRegionData } from '../redux/region/regionSlice';
+import CountriesList from '../components/countries/CountriesList';
+import styles from '../styles/Region.module.css';
+
 function ContinentPage() {
-  return <div>ContinentPage</div>;
+  const { regionId } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRegionData(regionId));
+  }, [dispatch, regionId]);
+
+  const {
+    regionData,
+    isLoadingRegionData,
+    errorLoadingRegionData,
+  } = useSelector((store) => store.region);
+
+  if (isLoadingRegionData) {
+    return <div className={styles.loading}>Loading countries...</div>;
+  }
+
+  if (errorLoadingRegionData) {
+    return (
+      <div className={styles.error}>
+        Error loading countries. Please try again later.
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <CountriesList
+        regionId={regionData.regionId}
+        regionName={regionData.regionName}
+        countries={regionData.countries}
+      />
+    </>
+  );
 }
 
 export default ContinentPage;
