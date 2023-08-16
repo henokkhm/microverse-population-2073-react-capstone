c0a44fd87f1742df0a8babd2e0df19f2db17f25a
Henok Kirubel <henokhm2@gmail.com>
2023-08-16 12:21:02 +0300

    Add routes for Home and ContinentPage pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 
 import { getRegions } from './redux/regions/regionsSlice';
+import ContinentPage from './pages/ContinentPage';
+import Home from './pages/Home';
 
 function App() {
   const dispatch = useDispatch();
@@ -15,6 +18,12 @@ function App() {
       <header>
         <h1>Population 2073</h1>
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:regionName" element={<ContinentPage />} />
+        </Routes>
+      </main>
     </div>
   );
 }
